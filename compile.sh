1314357d6fea06e407991cbc19e9d319fcd46521
wareya <wareya@gmail.com>
2025-08-06 20:17:45 -0400

    reorganize

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-clang -lgc -Oz main.c -flto -Wl,--gc-sections -fuse-ld=lld -Wall -Wextra -s -mllvm --inline-threshold=100
+clang -lgc -Oz src/main.c -flto -Wl,--gc-sections -fuse-ld=lld -Wall -Wextra -s -mllvm --inline-threshold=100
