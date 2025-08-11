41a7d696b9562b192be055e8f6a3614885f9a1d8
wareya <wareya@gmail.com>
2025-08-11 14:05:08 -0400

    get rid of libm

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-clang -std=c23 -lgc -Oz src/main.c -flto -Wl,--gc-sections -fuse-ld=lld -Wall -Wextra -s -mllvm --inline-threshold=100
+clang -std=c23 -lgc -Oz src/main.c -flto -Wl,--gc-sections -fuse-ld=lld -Wall -Wextra -s -fno-math-errno -Xclang -target-feature -Xclang +slow-unaligned-mem-16 -mllvm --inline-threshold=100
--- test_aot.sh
+++ test_aot.sh
@@ -1,4 +1,4 @@
 ./a.out $1 > asdfasdf.c \
-&& clang -O3 -g -ggdb  -Isrc asdfasdf.c -o b.out -lgc -lm \
+&& clang -O3 -g -ggdb  -Isrc asdfasdf.c -o b.out -lgc -fno-math-errno \
 	-flto -Xclang -target-feature -Xclang +slow-unaligned-mem-16 \
 && time ./b.out
